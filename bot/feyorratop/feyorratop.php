9deb5c07bff56b73c2d152d462793891b532e094
Gamamoch <gamamoch@gmail.com>
2026-05-14 18:42:20 +0700

    Set claim variable to false
    Changed claim variable from true to false.

--- bot/feyorratop/feyorratop.php
+++ bot/feyorratop/feyorratop.php
@@ -28,7 +28,7 @@ $limit = false;
 $shortlink = false;
 $SLDONE = false;
 $skipped = [];
-$claim = true;
+$claim = false;
 $can_withdraw = true;
 while (true) {
     $max = 7;
@@ -91,7 +91,7 @@ while (true) {
                 _sle(30);
                 continue;
             }
-            var_dump($ve); var_dump($po); die;
+            
             $alert_d = scraper::_xP($ve, "//div[contains(@class, 'alert-danger')]");
             if (!empty($alert_d)) {
                 $msg = $alert_d[0];
